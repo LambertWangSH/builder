ea784dd68e834a77eef2612b6aa66f5100bf5a61
LambertWangSH <41358648+LambertWangSH@users.noreply.github.com>
2026-05-05 13:04:41 +0800

    Update customizer.sh

--- devices/gk7205v210_lite_vixand-ivg-g4h/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7205v210_lite_vixand-ivg-g4h/general/overlay/usr/share/openipc/customizer.sh
@@ -10,21 +10,21 @@ fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/g
 #
 # Set custom majestic settings
 #
-cli -s .isp.blkCnt 5
+cli -s .isp.blkCnt 1
 cli -s .image.mirror true
 cli -s .image.flip true
 cli -s .video0.codec h264
-cli -s .osd.enabled true
-cli -s .osd.template "OpenIPC | %F %T %Z"
-cli -s .audio.enabled true
-cli -s .audio.volume 50
+cli -s .osd.enabled false
+#cli -s .osd.template "OpenIPC | %F %T %Z"
+#cli -s .audio.enabled true
+#cli -s .audio.volume 50
 #cli -s .audio.speakerPin 55
-cli -s .nightMode.irCutPin1 9              # or vice versa
-cli -s .nightMode.irCutPin2 8
-cli -s .nightMode.backlightPin 16          # or 14
-#cli -s .nightMode.lightMonitor true
-cli -s .nightMode.minThreshold 2000
-cli -s .nightMode.maxThreshold 14000
+#cli -s .nightMode.irCutPin1 9              # or vice versa
+#cli -s .nightMode.irCutPin2 8
+#cli -s .nightMode.backlightPin 16          # or 14
+##cli -s .nightMode.lightMonitor true
+#cli -s .nightMode.minThreshold 2000
+#cli -s .nightMode.maxThreshold 14000
 #
 #
 # Set wlan device and credentials if need
@@ -36,6 +36,11 @@ cli -s .nightMode.maxThreshold 14000
 adduser viewer -s /bin/false -D -H
 echo viewer:123456 | chpasswd
 #
+if [ -f /etc/majestic.yaml ]; then
+    # 确保配置文件中音频被关闭
+    sed -i '/^audio:/,/enabled:/ s/enabled: .*/enabled: false/' /etc/majestic.yaml
+    echo "Audio disabled in majestic.yaml"
+fi
 
 exit 0
 
